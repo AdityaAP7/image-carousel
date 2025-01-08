1ee01fc812e0f9f2a856932c041d4ed342ddb46e
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-08 23:19:16 +0530

    Added Carousel logic

--- Carousel.jsx
+++ Carousel.jsx
@@ -2,4 +2,6 @@ import React, { useState, useEffect } from "react";
 function Carousel({ images = [], autoPlay = true, interval = 3000 }) {const [currentIndex, setCurrentIndex] = useState(0);  useEffect(() => { let slideInterval; if (autoPlay) {  slideInterval = setInterval(() => {handleNext();}, interval);
 }return () => {
     clearInterval(slideInterval);
-  };  }, [currentIndex, autoPlay, interval]);const handlePrevious = () => { setCurrentIndex((prevIndex) =>
+  };  }, [currentIndex, autoPlay, interval]);const handlePrevious = () => { setCurrentIndex((prevIndex) =>  prevIndex === 0 ? images.length - 1 : prevIndex - 1
+  );
+};
