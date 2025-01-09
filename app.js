1a12359487209286932c7e51ba6eafbd3aa99eed
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-09 23:35:24 +0530

    Added app.js logic

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 import React from "react";
 import Carousel from "./Carousel";
-function App() {
+function App() { const sampleImages = [
+    "https://via.placeholder.com/600x400/ff7f7f/333333?text=Slide+1",
+    "https://via.placeholder.com/600x400/7fffd4/333333?text=Slide+2",
+    "https://via.placeholder.com/600x400/87cefa/333333?text=Slide+3",
+    "https://via.placeholder.com/600x400/f2bfff/333333?text=Slide+4"
+  ];
