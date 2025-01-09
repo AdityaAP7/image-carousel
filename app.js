a3abd2ded6df24863fc6410dd555c859a215dea9
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-09 23:35:35 +0530

    Added app.js logic

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ function App() { const sampleImages = [
     "https://via.placeholder.com/600x400/7fffd4/333333?text=Slide+2",
     "https://via.placeholder.com/600x400/87cefa/333333?text=Slide+3",
     "https://via.placeholder.com/600x400/f2bfff/333333?text=Slide+4"
-  ];
+  ];return (
+    <div className="App">
+      <h1>My Image Carousel</h1>
+      <Carousel images={sampleImages} autoPlay={true} interval={2000} />
+    </div>
+  );
+}
+
+export default App;
