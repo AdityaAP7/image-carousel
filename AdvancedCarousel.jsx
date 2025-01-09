5cf6b3bf21bf98a7202f60d97ba5bfbfa650e41a
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-09 23:37:09 +0530

    Advanced features

--- AdvancedCarousel.jsx
+++ AdvancedCarousel.jsx
@@ -3,3 +3,12 @@ import React, { useState, useEffect, useRef } from "react";
 // Example CSS classes for transitions, dot & thumbnail styling.
 // You can place this in "AdvancedCarousel.css" (or inline styles if you prefer).
 // See below for example CSS.
+
+function AdvancedCarousel({
+    images = [],
+    autoPlay = true,
+    interval = 3000,
+    showDots = true,
+    showThumbnails = true,
+    transitionType = "fade", // 'fade' or 'slide'
+  }) {
