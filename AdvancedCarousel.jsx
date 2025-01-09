51c55d9fc4fa7eb43d69ed4f750ba7df061785b5
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-09 23:39:25 +0530

    Advanced features

--- AdvancedCarousel.jsx
+++ AdvancedCarousel.jsx
@@ -34,4 +34,6 @@ function AdvancedCarousel({
         setCurrentIndex((prevIndex) =>
           prevIndex === 0 ? images.length - 1 : prevIndex - 1
         );
+      };  const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
       };
