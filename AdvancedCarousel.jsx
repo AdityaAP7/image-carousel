4c0aecf26d0ee969be90400bffb583e4c7d52d2e
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-07-05 23:54:04 +0530

    Update AdvancedCarousel.jsx

--- AdvancedCarousel.jsx
+++ AdvancedCarousel.jsx
@@ -1,4 +1,4 @@
-432143210987654321987654321trewq987654321654321qimport React, { useState, useEffect, useRef } from "react";
+5432143210987654321987654321trewq987654321654321qimport React, { useState, useEffect, useRef } from "react";
 function AdvancedCarousel({
   images = [],
   autoPlay = true,
