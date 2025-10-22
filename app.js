f28bc4a8f1329914c6107f8a6ad468675dbaea50
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-10-22 16:42:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-4321import React from "react";
+54321import React from "react";
 import Carousel from "./Carousel";
 function App() { const sampleImages = [
     "https://via.placeholder.com/600x400/ff7f7f/333333?text=Slide+1",
