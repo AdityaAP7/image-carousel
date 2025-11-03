8d1a1456ea02156e0775724e612e0fca3ce4a266
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-11-03 16:35:52 +0530

    Update Carousel.jack

--- Carousel.jsx
+++ Carousel.jsx
@@ -1,4 +1,4 @@
-210987654321import React, { useState, useEffect } from "react";
+3210987654321import React, { useState, useEffect } from "react";
 1function Carousel({ images = [], autoPlay = true, interval = 3000 }) {const [currentIndex, setCurrentIndex] = useState(0);  useEffect(() => { let slideInterval; if (autoPlay) {  slideInterval = setInterval(() => {handleNext();}, interval);
 }return () => {
     clearInterval(slideInterval);
